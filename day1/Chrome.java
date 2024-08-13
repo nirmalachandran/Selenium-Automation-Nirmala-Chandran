de950a59e2019189d74cdd7da38e301238bedac6
nirmalachandran <nirmala.chandran90@gmail.com>
2024-08-13 07:37:56 +0530

    Week1.Day1

---
+++ day1/Chrome.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//dataType variableName= value
+		//variable name which given inside main method- local variable
+		//variable -camel case
+				
+	
+		float version=91.0f;
+		String searchBox="Google";
+		boolean isBeta=false;
+		int releaseYear=2008;
+		char shortcutKey='C';
+		
+
+		System.out.println("Browser Version: "+version);
+		System.out.println("Browswer Name: "+searchBox);
+		System.out.println("Is Beta: "+isBeta
+		+"\n"+"Release Year: "+releaseYear
+		+"\n"+"Shortcut Key: "+shortcutKey);
+
+	}
+
+}
---
+++ day1/Firefox.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//dataType variableName= value
+		//variable name which given inside main method- local variable
+		//variable -camel case
+		int releaseYear=1998;
+		String searchBox="firefox";
+		boolean isVisible=true;
+		char browserLog='f';
+		float version=100.2f;
+		
+
+		System.out.println("Release Year: "+releaseYear);
+		System.out.println("Browswer Name: "+searchBox);
+		System.out.println("Release Year: "+releaseYear
+		+"\n"+"Browser Log: "+browserLog
+		+"\n"+"Browser Version: "+version);
+
+	}
+
+}
